25283c6b13d43cadb3f7547bcf23f20b0b52704e
LeiseR <willy.leiser@gmail.com>
2019-10-29 16:28:17 -0300

    Navigation implementada Pagina inicial ok

--- src/pages/Inicio/Inicio.js
+++ src/pages/Inicio/Inicio.js
@@ -2,6 +2,11 @@ import React from 'react'
 import {View,Text,Image,TouchableOpacity} from 'react-native'
 import {images} from '../../utils/const'
 import Styles from './Styles'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import Mapa from '../Mapa/Mapa'
+import Noticias from'../Noticias/Noticias'
+import Programacao from '../Programacao/Programacao'
 
 class Inicio extends React.Component{
 
@@ -13,16 +18,16 @@ class Inicio extends React.Component{
                     <Text>Timer</Text>
                 </View>
                 <View style={Styles.menuBar}>
-                    <TouchableOpacity style={Styles.button} onPress={()=>null}>
-                        <Image sorce={images.icon}/>
+                    <TouchableOpacity style={Styles.button} onPress={()=>this.props.navigation.navigate('Noticias')}>
+                        <Image source={images.icon}/>
                         <Text>Noticias</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={()=>null}>
-                        <Image sorce={images.icon}/>
+                    <TouchableOpacity style={Styles.button} onPress={()=>this.props.navigation.navigate('Mapa')}>
+                        <Image source={images.icon}/>
                         <Text>Mapa</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity onPress={()=>null}>
-                        <Image sorce={images.icon}/>
+                    <TouchableOpacity style={Styles.button} onPress={()=>this.props.navigation.navigate('Programacao')}>
+                        <Image source={images.icon}/>
                         <Text>Programação</Text>
                     </TouchableOpacity>
                 </View>
@@ -31,4 +36,23 @@ class Inicio extends React.Component{
     }
 }
 
-export default Inicio
+const AppNavigator = createStackNavigator({
+    Inicio: {
+      screen: Inicio,
+    },
+    Mapa: {
+        screen: Mapa,
+     },
+    Noticias: {
+        screen: Noticias,
+    },
+    Programacao:{
+        screen: Programacao,
+    }
+  },
+  {
+    initialRouteName: 'Inicio',
+    headerMode: 'none'
+  })
+
+export default createAppContainer(AppNavigator)
