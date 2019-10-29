e0f2c0be91d3302d4f60d85bf3dfb57434301b16
LeiseR <willy.leiser@gmail.com>
2019-10-29 15:28:42 -0300

    Inicio Alterações

--- src/pages/Inicio/Inicio.js
+++ src/pages/Inicio/Inicio.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View,Text,Image} from 'react-native'
+import {View,Text,Image,TouchableOpacity} from 'react-native'
 import {images} from '../../utils/const'
 import Styles from './Styles'
 
@@ -8,7 +8,24 @@ class Inicio extends React.Component{
     render(){
         return(
             <View>
-                <Image source={images.banner}/>
+                <Image source={images.banner3x} style={Styles.banner}/>
+                <View style={Styles.timer}>
+                    <Text>Timer</Text>
+                </View>
+                <View style={Styles.menuBar}>
+                    <TouchableOpacity style={Styles.button} onPress={()=>null}>
+                        <Image sorce={images.icon}/>
+                        <Text>Noticias</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>null}>
+                        <Image sorce={images.icon}/>
+                        <Text>Mapa</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={()=>null}>
+                        <Image sorce={images.icon}/>
+                        <Text>Programação</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
--- src/pages/Inicio/Styles.js
+++ src/pages/Inicio/Styles.js
@@ -1,7 +1,32 @@
 import {StyleSheet, Dimensions} from 'react-native'
+import {colors} from '../../utils/const'
+
+const valueWidth=Dimensions.get('screen').width
+const valueHeight=Dimensions.get('screen').height
 
 const Styles=StyleSheet.create({
+    banner:{
+        width:valueWidth,
+        height:valueHeight/3*1.212121,
+    },
+    timer:{
+        width:valueWidth,
+        height:valueHeight/5,
+        borderStyle:'dashed',
+        borderColor:colors.def_blue,
+        borderWidth:4,
+        borderRadius:10,
+        justifyContent:'center',
+        alignItems:'center'
 
+    },
+    menuBar:{
+        justifyContent:'space-between',
+    },
+    button:{
+        width:valueWidth/4,
+        height:valueHeight/10
+    }
 })
 
 export default Styles
