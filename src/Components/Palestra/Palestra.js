318217e7f3ed6b8416a38b9e15db899de5f71138
LeiseR <willy.leiser@gmail.com>
2019-11-07 15:51:20 -0300

    roda de conversas

--- src/Components/Palestra/Palestra.js
+++ src/Components/Palestra/Palestra.js
@@ -10,23 +10,29 @@ class Palestra extends React.Component{
         super()
        
     }
-    thereIsModal(){
-        if(this.props.link){
-            return(<Text style={Styles.info}>Toque aqui para mais informações</Text>)
-        }else{
-            return null
-        }
-    }
+  
     render(){
-        return(
-            <TouchableOpacity onPress={()=> this.props.callback(this.props.link)} style={Styles.back}>
-                <Text style={Styles.title}>{this.props.title}</Text>
-                <View style={Styles.line}></View>
-                <Text style={Styles.nomeLocal}>{this.props.local}</Text>
-                <Text style={Styles.local}>Local</Text>
-                {this.thereIsModal()}
-            </TouchableOpacity>
-        )
+        if(this.props.link == 'minicurso' || this.props.link == 'roda'){
+            return(
+                <TouchableOpacity onPress={()=> this.props.callback(this.props.link)} style={Styles.back}>
+                    <Text style={Styles.title}>{this.props.title}</Text>
+                    <View style={Styles.line}></View>
+                    <Text style={Styles.nomeLocal}>{this.props.local}</Text>
+                    <Text style={Styles.local}>Local</Text>
+                    <Text style={Styles.info}>Toque aqui para mais informações</Text>
+                </TouchableOpacity>
+            )
+        }else{
+            return(
+                <View style={Styles.back}>
+                    <Text style={Styles.title}>{this.props.title}</Text>
+                    <View style={Styles.line}></View>
+                    <Text style={Styles.nomeLocal}>{this.props.local}</Text>
+                    <Text style={Styles.local}>Local</Text>
+                </View>
+            )
+        }
+       
     }
 }
 
